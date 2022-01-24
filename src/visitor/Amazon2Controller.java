0b4e956a7c4077003df0d65a23e9ce0dbfa55d49
AndrewCrisler <andrew.t.c000@gmail.com>
2022-01-24 07:20:01 -0600

    created initial javafx program

---
+++ src/visitor/Amazon2Controller.java
@@ -0,0 +1,21 @@
+/*
+ * Course:     SE 2811
+ * Term:       Winter 2020-21
+ * Assignment: Lab 4 Bees
+ * Author:     Andrew Crisler
+ * Date:       1/5
+ */
+
+package visitor;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class Amazon2Controller {
+
+    @FXML
+    public void initialize() {
+
+    }
+}
---
+++ src/visitor/Main.java
@@ -0,0 +1,27 @@
+package visitor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Amazon2.fxml"));
+        primaryStage.setTitle("The Future of Amazon");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setWidth(900.0);
+        primaryStage.setHeight(700.0);
+        primaryStage.show();
+    }
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
