a1475cdf2f0c25eb16304f9aa1f4661c098bd657
AndrewCrisler <andrew.t.c000@gmail.com>
2022-02-06 16:54:16 -0600

    added the abillity for the controller to pick users and visit products

--- src/visitor/Amazon2Controller.java
+++ src/visitor/Amazon2Controller.java
@@ -8,14 +8,67 @@
 
 package visitor;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Amazon2Controller {
 
     @FXML
-    public void initialize() {
+    public VBox mainDisplay;
 
+    @FXML
+    public MenuItem basicUserButton;
+    @FXML
+    public MenuItem primeUserButton;
+    @FXML
+    public MenuItem distributorButton;
+
+
+    @FXML
+    public void initialize() {
+        Collection<Product> products = createProductCatalog(); //create product list
+
+        basicUserButton.setOnAction((event) -> {
+            createInterface(new BasicShopper(), products);
+        });
+
+        primeUserButton.setOnAction((event) -> {
+            createInterface(new PremiumShopper(), products);
+        });
+
+        distributorButton.setOnAction((event) -> {
+            createInterface(new Distributor(), products);
+        });
+
+        createInterface(new BasicShopper(), products); //default to basic user upon startup
+    }
+
+    public Collection<Product> createProductCatalog() {
+        Collection<Product> products = new ArrayList<>();
+        products.add(new GPU());
+        products.add(new Potatoes());
+        products.add(new RentACarProduct());
+        return products;
+    }
+
+    public void createInterface(Shopper shopper, Collection<Product> products){
+        for(Product product : products){ //this is not an ideal way of doing this
+            if(product instanceof GPU){
+                shopper.visit((GPU) product);
+            } else if (product instanceof Potatoes) {
+                shopper.visit((Potatoes) product);
+            } else {
+                shopper.visit((RentACarProduct) product);
+            }
+        }
     }
 }
