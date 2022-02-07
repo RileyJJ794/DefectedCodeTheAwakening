6b2ae7936646c1dfbc60ef8db63b62266e182d3a
AndrewCrisler <andrew.t.c000@gmail.com>
2022-02-07 06:55:06 -0600

    added logout, login functionallity to the controller

--- src/visitor/Amazon2Controller.java
+++ src/visitor/Amazon2Controller.java
@@ -8,12 +8,11 @@
 
 package visitor;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -23,7 +22,21 @@ import java.util.Collection;
 public class Amazon2Controller {
 
     @FXML
-    public VBox mainDisplay;
+    public Pane mainDisplay;
+
+    private Pane logoutMenuSave;
+
+    @FXML
+    public Label welcomeLabel;
+
+    @FXML
+    public HBox buttonMenu;
+    @FXML
+    public Button logout;
+    @FXML
+    public Button shoppingCartBtn;
+    @FXML
+    public Label numProducts;
 
     @FXML
     public MenuItem basicUserButton;
@@ -32,24 +45,45 @@ public class Amazon2Controller {
     @FXML
     public MenuItem distributorButton;
 
-
     @FXML
     public void initialize() {
         Collection<Product> products = createProductCatalog(); //create product list
 
+        buttonMenu.setVisible(false); //no user currently logged in
+        logoutMenuSave = (Pane) mainDisplay.getChildren().get(0); //save the logout menu
+
+        logout.setOnAction((event) -> {
+            displayLogoutMenu();
+        });
+
         basicUserButton.setOnAction((event) -> {
-            createInterface(new BasicShopper(), products);
+            displayCatalogMenu("Basic User", new BasicShopper(), products);
         });
 
         primeUserButton.setOnAction((event) -> {
-            createInterface(new PremiumShopper(), products);
+            displayCatalogMenu("Prime User", new PremiumShopper(), products);
         });
 
         distributorButton.setOnAction((event) -> {
-            createInterface(new Distributor(), products);
+            displayCatalogMenu("Distributor", new Distributor(), products);
         });
+    }
 
-        createInterface(new BasicShopper(), products); //default to basic user upon startup
+    private void displayLogoutMenu() {
+        welcomeLabel.setText("Welcome to Amazon2");
+        buttonMenu.setVisible(false);
+        mainDisplay.getChildren().clear();
+        mainDisplay.getChildren().add(logoutMenuSave);
+    }
+
+    private void displayCatalogMenu (String user, Shopper shopper, Collection<Product> products){
+        welcomeLabel.setText("Welcome " + user);
+        buttonMenu.setVisible(true);
+        mainDisplay.getChildren().clear();
+        VBox productDisplay = new VBox();
+        productDisplay.setPrefSize(900., 660.);
+        mainDisplay.getChildren().add(productDisplay);
+        createCatalog(shopper, products, productDisplay);
     }
 
     public Collection<Product> createProductCatalog() {
@@ -60,9 +94,10 @@ public class Amazon2Controller {
         return products;
     }
 
-    public void createInterface(Shopper shopper, Collection<Product> products){
+    //FIXME - this needs to be formatted right
+    public void createCatalog(Shopper shopper, Collection<Product> products, Pane productDisplay){
         for(Product product : products){
-            mainDisplay.getChildren().add(product.accept(shopper).displayMain());
+            //productDisplay.getChildren().add(product.accept(shopper).displayMain());
         }
     }
 }
