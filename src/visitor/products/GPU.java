487d535da592678db7e2c919f68699f5aca3db9f
hampe754 <hampe@msoe.edu>
2022-02-09 11:04:06 -0600

    GPU attributes initialized

--- src/visitor/products/GPU.java
+++ src/visitor/products/GPU.java
@@ -1,5 +1,6 @@
 package visitor.products;
 
+import javafx.scene.image.Image;
 import visitor.shoppers.Shopper;
 import visitor.display.DisplayShell;
 
@@ -7,8 +8,45 @@ import visitor.display.DisplayShell;
  * A GPU will be prices by the amount
  */
 public class GPU implements Product {
+    private int price = 100000;
+    private int shippingTime = 8;
+    private String handlingType = "plain box";
+    private Image gpuImage;
+
     @Override
     public DisplayShell accept(Shopper shopper) {
         return shopper.visit(this);
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getShippingTime() {
+        return shippingTime;
+    }
+
+    public void setShippingTime(int shippingTime) {
+        this.shippingTime = shippingTime;
+    }
+
+    public String getHandlingType() {
+        return handlingType;
+    }
+
+    public void setHandlingType(String handlingType) {
+        this.handlingType = handlingType;
+    }
+
+    public Image getGpuImage() {
+        return gpuImage;
+    }
+
+    public void setGpuImage(Image gpuImage) {
+        this.gpuImage = gpuImage;
     }
 }
