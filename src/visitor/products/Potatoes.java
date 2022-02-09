5ec565c7be3d6d49e39d1d1795936cce50a1b662
24jensenr <jensenr@msoe.edu>
2022-02-09 11:08:46 -0600

    added getters

--- src/visitor/products/Potatoes.java
+++ src/visitor/products/Potatoes.java
@@ -7,9 +7,23 @@ import visitor.display.DisplayShell;
  * The potatoes will be priced by the pound
  */
 public class Potatoes implements Product {
-
+    private int pricePerPound = 1;
+    private int shippingTime = 10;
+    private String handlingType = "plain box";
     @Override
     public DisplayShell accept(Shopper shopper) {
         return shopper.visit(this);
+    }
+
+    public int getPricePerPound() {
+        return pricePerPound;
+    }
+
+    public int getShippingTime() {
+        return shippingTime;
+    }
+
+    public String getHandlingType() {
+        return handlingType;
     }
 }
--- src/visitor/products/RentACarProduct.java
+++ src/visitor/products/RentACarProduct.java
@@ -7,8 +7,23 @@ import visitor.display.DisplayShell;
  * The RentACarProduct will be priced by the time it is rented (per hour or day)
  */
 public class RentACarProduct implements Product {
+    private int pricePerDay =  250;
+    private int shippingTime = 10;
+    private String handlingType = "plain box";
     @Override
     public DisplayShell accept(Shopper shopper) {
         return shopper.visit(this);
+    }
+
+    public int getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public int getShippingTime() {
+        return shippingTime;
+    }
+
+    public String getHandlingType() {
+        return handlingType;
     }
 }
